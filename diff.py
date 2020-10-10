5ce4268375b5b04c7d40cf6d94646fe9435fec29
Leonardo Gresta Paulino Murta <leomurta@ic.uff.br>
2020-10-10 02:43:23 -0300

    Adds a first diff implementation and two files for testing.
    This implementation is based on dynamic programming, with O(n^2) complexity and no extra optimizations.
    The adopted granularity in this implementation is lines.
    It prints the diff using colors to represent changes. Green represents added lines and red represents deleted lines.

---
+++ diff.py
@@ -0,0 +1,60 @@
+import argparse
+
+
+def read(filename):
+    with open(filename) as f:
+        return [None] + f.read().split('\n')
+
+
+def lcs(seq1, seq2):
+    matrix = [[0 for i in range(len(seq2))] for j in range(len(seq1))]
+
+    for i in range(1, len(seq1)):
+        for j in range(1, len(seq2)):
+            e1 = seq1[i]
+            e2 = seq2[j]
+
+            if e1 == e2:
+                matrix[i][j] = matrix[i-1][j-1] + 1
+            else:
+                matrix[i][j] = max(matrix[i][j-1], matrix[i-1][j])
+
+    return matrix
+
+
+def write(seq1, seq2, matrix):
+    RESET = '\033[0m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+
+    i = len(seq1) - 1
+    j = len(seq2) - 1
+    diff = []
+    while i > 0 or j > 0:
+        if i > 0 and j > 0 and seq1[i] == seq2[j]:
+            diff.append(seq1[i])
+            i, j = i-1, j-1
+        elif i > 0 and (j == 0 or matrix[i][j-1] < matrix[i-1][j]):
+            diff.append(RED + seq1[i] + RESET)
+            i -= 1
+        else:
+            diff.append(GREEN + seq2[j] + RESET)
+            j -= 1
+
+    print('\n'.join(reversed(diff)))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Compares two files.')
+    parser.add_argument('file1', help='name of the first file')
+    parser.add_argument('file2', help='name of the second file')
+    args = parser.parse_args()
+
+    seq1 = read(args.file1)
+    seq2 = read(args.file2)
+    matrix = lcs(seq1, seq2)
+    write(seq1, seq2, matrix)
+
+
+if __name__ == "__main__":
+    main()
